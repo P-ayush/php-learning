60d30f02ec965e06f5e4c6404c7ee0856cf2a989
Ayush gAMER <ayushprasad481@gmail.com>
2023-01-10 21:42:35 +0530

    learned image tag

---
+++ image-html.php
@@ -0,0 +1,8 @@
+<!doctype html>
+    <html>
+       
+            <body>
+                <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.gettyimages.in%2Fphotos%2Fnew-delhi&psig=AOvVaw2bTHMP5ZSsLhouDGGerdl4&ust=1673453193512000&source=images&cd=vfe&ved=0CA8QjRxqFwoTCLiSwYmxvfwCFQAAAAAdAAAAABAE" alt="this is an image">
+</body>
+
+</html>
