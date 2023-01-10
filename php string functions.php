7772c4062cc5eadc98e30b7bbc5b855407ef6c20
Ayush gAMER <ayushprasad481@gmail.com>
2023-01-10 21:25:13 +0530

    learned string functions and variables

---
+++ php string functions.php
@@ -0,0 +1,7 @@
+<?php
+echo strlen("Hello world!\n"); 
+echo str_word_count("Hello world!\n");
+echo strrev("Hello world!\n");
+echo strpos("Hello world!", "world\n");
+echo str_replace("world", "Dolly", "Hello world!"); 
+?>
---
+++ variable declaration.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<!DOCTYPE html>
+
+<?php
+$x=5;
+$y=6;
+echo "$x+$y";
+?>
+
+</body>
+</html>
